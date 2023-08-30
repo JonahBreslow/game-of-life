1cdb636a91055ef8665e1ed4ed515dcb5ba8c12e
Jonah Breslow <jonah.breslow@mindbodyonline.com>
2023-08-30 12:55:20 -0700

    conways game of life now works

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ var smallBoard = document.getElementById("smallBoard");
 var mediumBoard = document.getElementById("mediumBoard");
 var largeBoard = document.getElementById("largeBoard");
 var resetBoardBtn = document.getElementById("resetBoard");
+var startGameBtn = document.getElementById("startGame");
 var gridContainer = document.getElementById("grid");
+var startText = document.getElementById("startText");
 var r = document.querySelector(":root");
 var gridItems = document.querySelectorAll(".grid-item");
 var isMouseDown = false;
 // EVENT LISTENERS
 smallBoard.addEventListener("click", function () {
     clearGrid();
-    createGrid(16, 16);
+    createGrid(6, 6);
     gridItems = document.querySelectorAll(".grid-item");
 });
 mediumBoard.addEventListener("click", function () {
@@ -35,7 +37,6 @@ gridContainer.addEventListener("mouseover", function (event) {
     var target = event.target;
     if (target.classList.contains("grid-item")) {
         if (isMouseDown) {
-            console.log("hovered");
             target.classList.add("hovered");
         }
     }
@@ -49,26 +50,129 @@ gridContainer.addEventListener("mousedown", function (event) {
 resetBoardBtn.addEventListener("click", function () {
     resetGrid();
 });
+startGameBtn.addEventListener("click", function () {
+    gameOfLife();
+});
 // FUNCTIONS
 function createGrid(rows, cols) {
     r.style.setProperty("--grid-rows", rows.toString());
     r.style.setProperty("--grid-cols", cols.toString());
     for (var c = 0; c < rows * cols; c++) {
         var cell = document.createElement("div");
+        cell.id = c.toString();
         gridContainer.appendChild(cell).className = "grid-item aspect-100";
     }
 }
 function clearGrid() {
-    var gridItems = document.querySelectorAll(".grid-item");
     gridItems.forEach(function (item) {
         item.remove();
     });
+    startText.remove();
 }
 function resetGrid() {
-    var gridItems = document.querySelectorAll(".grid-item");
     gridItems.forEach(function (item) {
         item.classList.remove("hovered");
     });
 }
-clearGrid();
-createGrid(16, 16);
+function gameOfLife() {
+    var nAliveCells = countAliveCells();
+    var value = 1;
+    function performIterations(iterationsLeft) {
+        console.log("iterationsLeft: ".concat(iterationsLeft));
+        console.log("value: ".concat(value));
+        if (iterationsLeft === 0 || value === 0) {
+            return;
+        }
+        setTimeout(function () {
+            value = oneEvolution();
+            nAliveCells = countAliveCells();
+            performIterations(iterationsLeft - 1);
+        }, 100);
+    }
+    performIterations(10000); // Perform 3 iterations with a delay between each
+}
+function countAliveCells() {
+    var nAliveCells = 0;
+    gridItems.forEach(function (item) {
+        nAliveCells += item.classList.contains("hovered") ? 1 : 0;
+    });
+    return nAliveCells;
+}
+function oneEvolution() {
+    var grid = [
+        parseInt(r.style.getPropertyValue("--grid-rows")),
+        parseInt(r.style.getPropertyValue("--grid-cols")),
+    ];
+    var turnOn = [];
+    var turnOff = [];
+    gridItems.forEach(function (item) {
+        var index = parseInt(item.id);
+        var _a = indexToRowCol(index, grid), row = _a[0], col = _a[1];
+        var nNeighbors = countNeighbors(row, col, grid);
+        if (item.classList.contains("hovered")) {
+            if (nNeighbors < 2 || nNeighbors > 3) {
+                turnOff.push(index);
+            }
+        }
+        else {
+            if (nNeighbors === 3) {
+                turnOn.push(index);
+            }
+        }
+    });
+    if (turnOff.length === 0 && turnOn.length === 0) {
+        return 0;
+    }
+    else {
+        var returnVal = turnOn.length + turnOff.length;
+        turnOn.forEach(function (index) {
+            var cell = document.getElementById(index.toString());
+            cell.classList.add("hovered");
+        });
+        turnOff.forEach(function (index) {
+            var cell = document.getElementById(index.toString());
+            cell.classList.remove("hovered");
+        });
+        return returnVal;
+    }
+}
+function countNeighbors(row, col, grid) {
+    var index = row * grid[0].length + col;
+    var neighborOffset = [
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [0, -1],
+        [0, 1],
+        [1, -1],
+        [1, 0],
+        [1, 1], // bottom right
+    ];
+    var count = 0;
+    for (var _i = 0, neighborOffset_1 = neighborOffset; _i < neighborOffset_1.length; _i++) {
+        var offset = neighborOffset_1[_i];
+        var neighborRow = row + offset[0];
+        var neighborCol = col + offset[1];
+        if (isCellOnGrid(neighborRow, grid, neighborCol)) {
+            var neighborIndex = neighborRow * grid[0] + neighborCol;
+            var neighborCell = document.getElementById(neighborIndex.toString());
+            if (neighborIndex >= 0 &&
+                neighborIndex < grid[0] * grid[1] &&
+                neighborCell.classList.contains("hovered")) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+function isCellOnGrid(neighborRow, grid, neighborCol) {
+    return (neighborRow >= 0 &&
+        neighborRow <= grid[0] &&
+        neighborCol >= 0 &&
+        neighborCol <= grid[1]);
+}
+function indexToRowCol(index, grid) {
+    var row = Math.floor(index / grid[0]);
+    var col = index % grid[1];
+    return [row, col];
+}
--- script.ts
+++ script.ts
@@ -5,28 +5,30 @@ const smallBoard = document.getElementById("smallBoard") as HTMLElement;
 const mediumBoard = document.getElementById("mediumBoard") as HTMLElement;
 const largeBoard = document.getElementById("largeBoard") as HTMLElement;
 const resetBoardBtn = document.getElementById("resetBoard") as HTMLElement;
+const startGameBtn = document.getElementById("startGame") as HTMLElement;
 const gridContainer = document.getElementById("grid") as HTMLElement;
+const startText = document.getElementById("startText") as HTMLElement;
 const r = document.querySelector(":root") as HTMLElement;
 let gridItems = document.querySelectorAll(".grid-item");
 let isMouseDown = false;
 
 // EVENT LISTENERS
 smallBoard.addEventListener("click", () => {
-    clearGrid();
-    createGrid(16, 16);
-    gridItems = document.querySelectorAll(".grid-item");
+  clearGrid();
+  createGrid(6, 6);
+  gridItems = document.querySelectorAll(".grid-item");
 });
 
 mediumBoard.addEventListener("click", () => {
-    clearGrid();
-    createGrid(45, 45);
-    gridItems = document.querySelectorAll(".grid-item");
+  clearGrid();
+  createGrid(45, 45);
+  gridItems = document.querySelectorAll(".grid-item");
 });
 
 largeBoard.addEventListener("click", () => {
-    clearGrid();
-    createGrid(69, 69);
-    gridItems = document.querySelectorAll(".grid-item");
+  clearGrid();
+  createGrid(69, 69);
+  gridItems = document.querySelectorAll(".grid-item");
 });
 
 gridContainer.addEventListener("mousedown", (event) => {
@@ -41,21 +43,24 @@ gridContainer.addEventListener("mouseover", (event) => {
   const target = event.target as HTMLElement;
   if (target.classList.contains("grid-item")) {
     if (isMouseDown) {
-      console.log("hovered");
       target.classList.add("hovered");
     }
   }
 });
 
 gridContainer.addEventListener("mousedown", (event) => {
-    const target = event.target as HTMLElement;
-    if (target.classList.contains("grid-item")) {
-        target.classList.add("hovered");
-    }
+  const target = event.target as HTMLElement;
+  if (target.classList.contains("grid-item")) {
+    target.classList.add("hovered");
+  }
 });
 
 resetBoardBtn.addEventListener("click", () => {
-    resetGrid();
+  resetGrid();
+});
+
+startGameBtn.addEventListener("click", () => {
+  gameOfLife();
 });
 
 // FUNCTIONS
@@ -64,29 +69,134 @@ function createGrid(rows: number, cols: number) {
   r.style.setProperty("--grid-cols", cols.toString());
   for (let c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
+    cell.id = c.toString();
     gridContainer.appendChild(cell).className = "grid-item aspect-100";
   }
 }
 
 function clearGrid() {
-  const gridItems = document.querySelectorAll(".grid-item");
   gridItems.forEach((item) => {
     item.remove();
   });
+  startText.remove();
 }
 
 function resetGrid() {
-  const gridItems = document.querySelectorAll(".grid-item");
   gridItems.forEach((item) => {
     item.classList.remove("hovered");
   });
 }
 
-// TODO: Implement game of life
 function gameOfLife() {
-  return 
-
+  let nAliveCells = countAliveCells();
+  let value = 1; 
+  function performIterations(iterationsLeft: number) {
+    console.log(`iterationsLeft: ${iterationsLeft}`)
+    console.log(`value: ${value}`)
+    if (iterationsLeft === 0 || value === 0) {
+      return;
+    }
+    setTimeout(() => {
+      value = oneEvolution();
+      nAliveCells = countAliveCells();
+      performIterations(iterationsLeft - 1);
+    }, 100); 
+  }
+  
+  performIterations(10000); // Perform 3 iterations with a delay between each
 }
 
-clearGrid();
-createGrid(16, 16);
+function countAliveCells(): number {
+  let nAliveCells = 0;
+  gridItems.forEach((item) => {
+    nAliveCells += item.classList.contains("hovered") ? 1 : 0;
+  });
+  return nAliveCells;
+}
+
+function oneEvolution(): number {
+  const grid = [
+    parseInt(r.style.getPropertyValue("--grid-rows")!),
+    parseInt(r.style.getPropertyValue("--grid-cols")!),
+  ];
+  const turnOn: number[] = [];
+  const turnOff: number[] = [];
+  gridItems.forEach((item) => {
+    const index = parseInt(item.id);
+    const [row, col] = indexToRowCol(index, grid);
+    const nNeighbors = countNeighbors(row, col, grid);
+    if (item.classList.contains("hovered")) {
+      if (nNeighbors < 2 || nNeighbors > 3) {
+        turnOff.push(index);
+      }
+    } else {
+      if (nNeighbors === 3) {
+        turnOn.push(index);
+      }
+    }
+  });
+
+  if (turnOff.length === 0 && turnOn.length === 0) {
+    return 0;
+  } else {
+    const returnVal = turnOn.length + turnOff.length;
+    turnOn.forEach((index) => {
+      const cell = document.getElementById(index.toString()) as HTMLElement;
+      cell.classList.add("hovered");
+    });
+    turnOff.forEach((index) => {
+      const cell = document.getElementById(index.toString()) as HTMLElement;
+      cell.classList.remove("hovered");
+    });
+    return returnVal;
+  }
+}
+
+function countNeighbors(row: number, col: number, grid: any) {
+  const index = row * grid[0].length + col;
+  const neighborOffset = [
+    [-1, -1], // top left
+    [-1, 0], // top
+    [-1, 1], // top right
+    [0, -1], // left
+    [0, 1], // right
+    [1, -1], // bottom left
+    [1, 0], // bottom
+    [1, 1], // bottom right
+  ];
+
+  let count = 0;
+  for (const offset of neighborOffset) {
+    const neighborRow = row + offset[0];
+    const neighborCol = col + offset[1];
+    if (isCellOnGrid(neighborRow, grid, neighborCol)) {
+      const neighborIndex = neighborRow * grid[0] + neighborCol;
+      const neighborCell = document.getElementById(
+        neighborIndex.toString()
+      ) as HTMLElement;
+      if (
+        neighborIndex >= 0 &&
+        neighborIndex < grid[0] * grid[1] &&
+        neighborCell.classList.contains("hovered")
+      ) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+function isCellOnGrid(neighborRow: number, grid: any, neighborCol: number) {
+  return (
+    neighborRow >= 0 &&
+    neighborRow <= grid[0] &&
+    neighborCol >= 0 &&
+    neighborCol <= grid[1]
+  );
+}
+
+function indexToRowCol(index: number, grid: number[]): number[] {
+  const row = Math.floor(index / grid[0]);
+  const col = index % grid[1];
+  return [row, col];
+}
